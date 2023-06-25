1119efed2b3b6ec90b0e205bd600e49c12af593b
Mark Morfe <40056285+markpogi0121@users.noreply.github.com>
2023-06-25 02:21:01 +0000

    changed test_add to fail

--- calculator.py
+++ calculator.py
@@ -21,8 +21,8 @@ def div(a, b):
 
 if __name__ == "__main__":
     # Declare variable and set default values
-    a = 4
-    b = 2
+    a = 5
+    b = 3
     print("Sum of " + str(a) + " and " + str(b) + " is ", add(a, b))
     print("Difference of " + str(a) + " and " + str(b) + " is ", sub(a, b))
     print("Product of " + str(a) + " and " + str(b) + " is ", mul(a, b))
